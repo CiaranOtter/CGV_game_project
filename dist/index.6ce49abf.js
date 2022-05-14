09e65918cfcafaf630770bfbe08266f22951ae1d
FURAHKAY <furahakabeya.fk@gmail.com>
2022-05-14 11:58:56 +0200

    adjustments to start menu

--- dist/index.6ce49abf.js
+++ dist/index.6ce49abf.js
@@ -526,6 +526,32 @@ function hmrAcceptRun(bundle, id) {
 }
 
 },{}],"8YfsX":[function(require,module,exports) {
+function hide(x, y, z, w) {
+    x.style.display = 'none';
+    y.style.display = 'none';
+    z.style.display = 'none';
+}
+function showScore(w) {
+    w.innerHTML;
+    w = document.getElementById('p').innerHTML = "You Won!";
+    w.style.color = 'green';
+}
+let btnText = document.getElementById('btnText');
+let btnText2 = document.getElementById('btnText2');
+let btnText3 = document.getElementById('btnText3');
+let score = document.getElementById('p').innerHTML;
+btnText.addEventListener('click', ()=>{
+    hide(btnText, btnText2, btnText3);
+});
+btnText2.addEventListener('click', ()=>{
+    hide(btnText, btnText2, btnText3);
+    showScore(score);
+});
+btnText3.addEventListener('click', ()=>{
+    hide(btnText, btnText2, btnText3);
+    scene.clear();
+});
+const { BackSide  } = require("three");
 const THREE = require("three");
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
--- threejs_test/script.js
+++ threejs_test/script.js
@@ -1,7 +1,44 @@
+function hide(x, y, z, w) {
+    x.style.display = 'none';
+    y.style.display = 'none';
+    z.style.display = 'none';
+
+
+}
+function showScore(w) {
+    w.innerHTML;
+    w = document.getElementById('p').innerHTML = "You Won!"
+    w.style.color = 'green';
+
+
+
+}
+let btnText = document.getElementById('btnText');
+let btnText2 = document.getElementById('btnText2');
+let btnText3 = document.getElementById('btnText3');
+let score = document.getElementById('p').innerHTML;
+
+btnText.addEventListener('click', () => {
+    hide(btnText, btnText2, btnText3);
+
+});
+btnText2.addEventListener('click', () => {
+
+    hide(btnText, btnText2, btnText3);
+    showScore(score);
+
+});
+btnText3.addEventListener('click', () => {
+
+    hide(btnText, btnText2, btnText3);
+    scene.clear();
+});
+
+const { BackSide } = require("three");
 const THREE = require("three")
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth/ window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer();
 
@@ -9,17 +46,17 @@ renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.append(renderer.domElement);
 
 const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial( { color:  0x00ff00} );
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(geometry, material);
-scene.add( cube );
+scene.add(cube);
 
 camera.position.z = 5;
 
 function animate() {
-    requestAnimationFrame( animate );
+    requestAnimationFrame(animate);
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
-    renderer.render( scene, camera); 
+    renderer.render(scene, camera);
 }
 
 animate();
